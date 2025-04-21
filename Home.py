96afbfa0bb9c78d9c24dc27e6ae0edbd8685edc1
Charlie <95353746+Ch-arlie7@users.noreply.github.com>
2025-04-21 15:41:41 +0100

    updated layout

--- Home.py
+++ Home.py
@@ -13,7 +13,8 @@ def sync_sheet():
     '''
     conn = st.connection("gsheets", type=GSheetsConnection)
     st.session_state['df'] = conn.read(worksheet='Data', ttl=1)
-    st.session_state['exercises'] = conn.read(worksheet='Exercises', ttl=1)
+    st.session_state['exercises'] = conn.read(
+        worksheet='Exercises', ttl=1)
 
 
 def signed_in():
@@ -41,8 +42,11 @@ def push_data(entry):
     entry.insert(0, 'Name', [st.session_state['name']
                  for x in range(len(entry.index))], allow_duplicates=True)
     sync_sheet()
-    st.session_state['df'] = pd.concat(
-        [st.session_state['df'], entry], ignore_index=True)
+    if len(st.session_state['df'].index) > 0:
+        st.session_state['df'] = pd.concat(
+            [st.session_state['df'], entry], ignore_index=True)
+    else:
+        st.session_state['df'] = entry
     conn = st.connection("gsheets", type=GSheetsConnection)
     conn.update(data=st.session_state['df'])
 
@@ -59,13 +63,13 @@ if 'key' not in st.session_state:
 if 'df' not in st.session_state or 'exercises' not in st.session_state:
     sync_sheet()
 
-exercises = [''] + st.session_state['exercises']['Exercises'].to_list()
+exercises = sorted([''] + st.session_state['exercises']['Exercises'].to_list())
 
 
 st.title('GymTrack')
 st.text('Lift tracking and analytics.')
 entry_tab, analytics_tab, account_tab = st.tabs(
-    [':pencil: Record', ':chart_with_upwards_trend: Stats', f':gear: Account {signed_in_checkmark()}'])
+    [':pencil: Record', ':chart_with_upwards_trend: Analytics', f':gear: Account {signed_in_checkmark()}'])
 
 with account_tab:
     if signed_in():
@@ -77,7 +81,7 @@ with account_tab:
         username = account_tab.text_input('Username', max_chars=10)
         sign_in = account_tab.button('Sign In')
         if sign_in:
-            st.session_state['name'] = username
+            st.session_state['name'] = username.lower()
             st.rerun()
 
 
@@ -111,19 +115,72 @@ with entry_tab:
             reset_key()
             st.rerun()
 
-        st.text('Recent Exercises')
-        recent_exercises = st.session_state['df'][st.session_state['df']
-                                                  ['Name'] == st.session_state['name']].sort_values(by=['Timestamp'], ascending=False).groupby('Exercise').first()
-        st.dataframe(recent_exercises)
+        # Exercises
 
-        st.text('Recent Sessions')
+        exercise_history = entry_tab.selectbox(
+            'Recent Exercises', options=exercises, index=None, placeholder='Choose an exercise')
+        exercise_history_df = st.session_state['df'].loc[(st.session_state['df']['Name'] == st.session_state['name']) & (
+            st.session_state['df']['Exercise'] == exercise_history)].sort_values(by=['Timestamp'], ascending=False)
+        exercise_history_df['Timestamp'] = exercise_history_df['Timestamp'].apply(
+            lambda x: x.split(' ')[0]
+        )
+        exercise_history_df = exercise_history_df[[
+            'Timestamp', 'Weight', 'Sets', 'Reps', 'Last-set', 'Effort']]
+        exercise_history_df = exercise_history_df.rename(
+            columns={'Timestamp': 'Date'})
+        if len(exercise_history_df.index) > 0:
+            st.dataframe(exercise_history_df.head(6), hide_index=True)
+
+        # recent_exercises = st.session_state['df'][st.session_state['df']
+        #                                           ['Name'] == st.session_state['name']].sort_values(by=['Timestamp'], ascending=False).groupby('Exercise').first()
+        # # 'Exercise' column auto included as the index.
+        # recent_exercises = recent_exercises[[
+        #     'Weight', 'Sets', 'Reps', 'Last-set', 'Effort']]
+        # if len(recent_exercises.index) > 0:
+        #     st.text('Recent Exercises')
+        #     st.dataframe(recent_exercises)
+
+        # Sessions
         # Separate only the Users' data, sort in order of most recent.
         recent_sessions = st.session_state['df'][st.session_state['df']
                                                  ['Name'] == st.session_state['name']].sort_values(by=['Timestamp'], ascending=False)
         # Convert timestamp column from "date time" to "date"
         recent_sessions['Timestamp'] = recent_sessions['Timestamp'].apply(
             lambda x: x.split(' ')[0])
+        # rename timestamp column
+        recent_sessions = recent_sessions.rename(columns={'Timestamp': 'Date'})
         # Convert Exercise column to show set of exercises on each date.
-        recent_sessions = recent_sessions.groupby('Timestamp')[
+        recent_sessions = recent_sessions.groupby('Date', sort=False)[
             'Exercise'].apply(set)
-        st.dataframe(recent_sessions)
+        if len(recent_sessions.index) > 0:
+            st.text('Recent Sessions')
+            st.dataframe(recent_sessions.head(6))
+
+
+with analytics_tab:
+    if not signed_in():
+        st.text('Sign In ↗️')
+    else:
+        st.write('TODO')
+
+        # # Raw DataFrame
+        # data = st.session_state['df'][st.session_state['df']['Name'] ==
+        #                               st.session_state['name']].sort_values(by=['Timestamp'], ascending=False)
+        # time_removed = data.copy()
+        # time_removed['Timestamp'] = time_removed['Timestamp'].apply(
+        #     lambda x: x.split(' ')[0])
+        # num_workouts = len(time_removed.groupby('Timestamp').count().index)
+        # general_df = pd.DataFrame({
+        #     'Total Workouts': [f'{num_workouts}']})
+        # options = ['Data', 'General', 'Exercise Specific']
+        # radio = analytics_tab.radio(
+        #     label='Stats', options=options, horizontal=True, index=None)
+        # if radio == 'Data':
+        #     st.dataframe(data, hide_index=True)
+        # elif radio == 'General':
+        #     st.dataframe(general_df, hide_index=True)
+        # elif radio == 'Exercise Specific':
+        #     exercise = analytics_tab.selectbox(
+        #         'Exercise:', options=data['Exercise'].unique(), placeholder='Choose an option')
+
+        #     st.dataframe()
